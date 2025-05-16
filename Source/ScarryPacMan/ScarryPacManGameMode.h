1ce3c8c1f5eb0ed150ad3b5bd0e3f87631d291e5
hcodename47 <hcodename47@gmail.com>
2025-05-16 09:32:48 +0900

    initial commit

---
+++ Source/ScarryPacMan/ScarryPacManGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ScarryPacManGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AScarryPacManGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AScarryPacManGameMode();
+};
+
+
+
